a44d4161b1adeea1cb7ec0192646010022eeaf77
aether <bharath.s.sada@gmail.com>
2024-02-22 23:06:03 +0000

    stable working app with rockylinux

--- webserver/configfiles/docker-entrypoint.sh
+++ webserver/configfiles/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# mkdir -p /run/php-fpm/
+mkdir -p /run/php-fpm/
 /usr/sbin/nginx
-/etc/init.d/php8.2-fpm start
-# /usr/local/sbin/php-fpm -F
+# /etc/init.d/php8.2-fpm start
+/usr/sbin/php-fpm -F
 # /usr/sbin/sshd -D
